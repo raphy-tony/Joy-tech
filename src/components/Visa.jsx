7cfe16f035e4bf827e705450ba9c031533454596
Anthony Raphael <raphaeloyaro@gmail.com>
2023-01-07 00:01:24 +0300

    Added motions on Visa.jsz

--- src/components/Visa.jsx
+++ src/components/Visa.jsx
@@ -2,9 +2,9 @@ import {bill } from "../assets";
 import styles, { layout } from "../style";
 
 import { motion } from 'framer-motion';
-import { TitleText, TypingText } from '../components';
+import { TitleText} from '../components';
 
-import { fadeIn, staggerContainer } from '../utils/motion';
+import { fadeIn, staggerContainer,planetVariants } from '../utils/motion';
 
 const Visa = () => (
   <motion.section id="visa"
@@ -14,14 +14,16 @@ const Visa = () => (
     viewport={{ once: false, amount: 0.25 }}
     className={layout.sectionReverse}
     >
-      <div className={layout.sectionImgReverse}>
+      <motion.div
+        variants={planetVariants('left')}
+        className={layout.sectionImgReverse}>
         <img src={bill} alt="Visa" className="w-[100%] h-[100%] relative z-[5]" />
 
         {/* gradient start */}
         <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
         <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
         {/* gradient end */}
-      </div>
+      </motion.div>
 
       <div className={layout.sectionInfo}>
       <TitleText
@@ -32,9 +34,11 @@ const Visa = () => (
         )}
         textStyles=" "
       />     
-        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        <motion.p
+          variants={fadeIn('up', 'tween', 0.3, 1)}
+          className={`${styles.paragraph} max-w-[470px] mt-5`}>
           We offer you a hassle-free visa application assistance and other passport services. From application procedures to legalization of your documents, we have got everything under control. With us, you can rest assured that you are in absolutely safe hands. We assist visa application to China, USA, United Kingdom, India, Dubai, Vietnam, Schengen countries and Most African countries
-        </p>
+        </motion.p>
       </div>
   </motion.section>
 );
