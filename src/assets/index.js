fcd48d9b742b5180a220a23f822cb1759bb8c67e
Anthony Raphael <raphaeloyaro@gmail.com>
2023-01-08 16:57:32 +0300

    rel icon added

--- src/assets/index.js
+++ src/assets/index.js
@@ -24,6 +24,7 @@ import people01 from "./people01.png";
 import people02 from "./people02.png";
 import people03 from "./people03.png";
 import laptop from "./laptop.png";
+import airplane from "./airplane.svg";
 
 export {
   airbnb,
@@ -52,4 +53,5 @@ export {
   people02,
   people03,
   laptop,
+  airplane,
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import styles from "../style";
 import { motion } from 'framer-motion';
 
 import { fadeIn, staggerContainer, zoomIn } from '../utils/motion';
-import { discount, laptop, robot } from "../assets";
+import { discount, laptop, airplane } from "../assets";
 
 const Hero = () => {
   return (
@@ -18,7 +18,7 @@ const Hero = () => {
           className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
         >
           <motion.div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
-            <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+            <img src={airplane} alt="airplane" className="w-[25px] h-[25px]" />
             <p className={`${styles.paragraph} ml-2`}>
               <span className="text-white">Best</span> Travel consultantion Services from the most trusted Agency{" "}
             </p>
